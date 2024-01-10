325e67dce88ff94db3ad203f2a5348bec77ab2c7
VDani3 <villacarmona.dani@gmail.com>
2024-01-10 20:41:45 +0100

    boton eliminar

---
+++ www/js/index.js
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    //document.getElementById('deviceready').classList.add('ready');
+
+    init();
+}
+
+function init() {
+    $("#addTask").click(addTask);
+}
+
+function addTask() {
+    let question = prompt("Task name:");
+    var delButton = $('<button id="delete">x</button>');
+    delButton.click(deleteLi);
+    delButton.css('float','right')
+    //$("#delete").click(al);
+    var newElem = $("<li>"+question+'</li>');
+    newElem.append(delButton);
+    $("ul").append(newElem);
+    $("ul").listview("refresh");
+}
+
+function deleteLi(e) {
+    var caller = e.target || e.srcElement;
+    $(caller).parent().remove();
+    $("ul").listview("refresh");
+}
